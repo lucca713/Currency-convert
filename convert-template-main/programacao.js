b0bc784816e01c04f3801cce77c2fee7b6597393
lucca <cerasommalucca@gmail.com>
2025-02-18 07:52:59 -0300

    Finalizando o projeto de conversão de moedas

--- convert-template-main/programacao.js
+++ convert-template-main/programacao.js
@@ -1,7 +1,15 @@
+//cotacoes do dia
+const USD = 5.71
+const EUR = 5.98
+const GBP = 7.20
 //area das variáveis
+
 const input_valor = document.querySelector("#amount")
 const select_moeda = document.querySelector("#currency")
 const form = document.querySelector("form")
+const footer = document.querySelector("main footer")
+const description = document.querySelector("#description")
+const result = document.querySelector("#result")
 //fim variaveis
 
 //Create area about capture just numbers in input 
@@ -15,9 +23,46 @@ input_valor.addEventListener("input",(e) =>{
 
 //outro modo para lidar com evento arrow function também
 //caputrando submite do formulario
+
 form.onsubmit = (event) =>{
     event.preventDefault()
 
-    console.log(select_moeda.value)
+    switch(select_moeda.value){
+        case "USD": 
+            convert(input_valor.value,USD,"$")
+            break
+        case "EUR":
+             convert(input_valor.value,EUR,"€")
+             break
+        case "GBP":
+             convert(input_valor.value,GBP,"£")
+             break
+    }
+
+    
 }
 
+//funcao converter moeda e colocar ela exibindo no footer
+
+function convert(amount, price, symbol){
+    try{
+    let total = amount * price
+
+     description.textContent = ` 1${symbol} = ${formatCurrencyBRL(price)} `   
+     
+     result.textContent = `${formatCurrencyBRL(total.toFixed(2))}`
+
+     footer.classList.add("show-result")
+   
+    } catch(error){
+        alert("erros inesperado na funcionalidade, por favor atualize a página e tente novamente")
+    }
+}
+
+//isso é padrao, como ele formata moeda em real
+function formatCurrencyBRL(value){
+    return Number(value).toLocaleString("pt-BR",{
+    style : "currency",
+    currency: "BRL"
+    })
+}
